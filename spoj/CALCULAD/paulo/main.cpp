6179c386452dfafe8be21820a0cbce9784466523
Paulo Roberto Urio <paulourio@gmail.com>
2012-03-24 17:55:58 -0300

    CALCULAD. Still didn't pass.
    Added a few more tests.
    Signed-off-by: Paulo Roberto Urio <paulourio@gmail.com>

--- spoj/CALCULAD/paulo/main.cpp
+++ spoj/CALCULAD/paulo/main.cpp
@@ -6,23 +6,21 @@
 #include <stack>
 
 using namespace std;
-float memory[26];
+double memory[26];
 #define MEMORY(chr)	memory[chr - 'a']
 #define ISNUM(chr)	'0' <= (chr) && (chr) <= '9'
 #define ISVAR(chr)	'a' <= (chr) && (chr) <= 'z'
 #define ISPAR(chr)	((chr) == '(' || (chr) == ')')
 #define ISOP(c)		((c) == '*' || (c) == '/' || (c) == '+' || (c) == '-')
-char buffer[301];
+char buffer[5000001];
 
 int prec(int op)
 {
 	switch (op) {
-	case '*':
-	case '/':
-		return 2;
-	case '+':
-	case '-':
-		return 1;
+	case '/': 
+	case '*': return 3;
+	case '+': 
+	case '-': return 1;
 	}
 	return -100;
 }
@@ -30,12 +28,9 @@ int prec(int op)
 enum {VARIAVEL, NUMERO, OPERADOR, PARENTESE};
 int token(int value)
 {
-	if (ISNUM(value))
-		return NUMERO;
-	if (ISVAR(value))
-		return VARIAVEL;
-	if (ISPAR(value))
-		return PARENTESE;
+	if (ISNUM(value))	return NUMERO;
+	if (ISVAR(value))	return VARIAVEL;
+	if (ISPAR(value))	return PARENTESE;
 	return OPERADOR;
 }
 
@@ -46,11 +41,10 @@ void desempilhar(void)
 {
 	char *top = ops.top();
 	ops.pop();
-	if (*top != '(')
-		pfix.push_back(top);
+	pfix.push_back(top);
 }
 
-float eval(char *ptr)
+double eval(char *ptr)
 {
 	pfix.clear();
 	while (*ptr != 0) {
@@ -63,17 +57,19 @@ float eval(char *ptr)
 			while (ISNUM(*ptr)) ptr++;
 			break;
 		case PARENTESE:
-			if (*ptr == '(')
+			if (*ptr == '(') {
 				ops.push(ptr);
-			else 
+			} else {
 				while (*ops.top() != '(')
 					desempilhar();
+				ops.pop();
+			}
 			ptr++;
 			break;
 		case OPERADOR:
 			while (!ops.empty()) {
 				char *top = ops.top();
-				if (ISPAR(*top) || prec(*ptr) - prec(*top) <= 0)
+				if (prec(*top) - prec(*ptr) < 0)
 					break;
 				desempilhar();
 			}
@@ -83,59 +79,47 @@ float eval(char *ptr)
 	while (!ops.empty())
 		desempilhar();
 
-	cout << "Posfixo: ";
+/*	cerr << "Posfixo: " ;
 	for (list<char *>::iterator i=pfix.begin(); i!=pfix.end(); i++)
-		cout << *(*i);
-	cout << endl << endl;
-
-	stack<float> pilha;
+		cerr << *(*i);
+	cerr << endl;
+*/
+	stack<double> pilha;
 	while (!pfix.empty()) {
 		char *t = pfix.front();
 		pfix.pop_front();
 
 		if (ISOP(*t)) {
-			float b = pilha.top(); pilha.pop();
-			float a = pilha.top(); pilha.pop();
-			float r;
-			cout << "Eval " << a << " " << *t << " " << b << endl;
+			double b = pilha.top(); pilha.pop();
+			double a = pilha.top(); pilha.pop();
+			double r;
 			switch(*t) {
 			case '*': r = a * b; break;
 			case '/': r = a / b; break;
 			case '+': r = a + b; break;
 			case '-': r = a - b; break;
 			}
+			cerr << "Resultado " << a << " " << *t << " " << b << " = " << r << endl;
 			pilha.push(r);
 			continue;
 		}
-		if (ISVAR(*t)) {
-			pilha.push( (float) MEMORY(*t) );
-			continue;
-		}
-		if (ISNUM(*t)) {
-			pilha.push( (float) strtol(t, NULL, 10) );
-			continue;
-		}
+		if (ISVAR(*t)) 
+			pilha.push( (double) MEMORY(*t) );
+		else if (ISNUM(*t)) 
+			pilha.push( (double) strtoul(t, NULL, 10) );
 
 	}
-	cout << "Result: " << pilha.top() << endl;
 	return pilha.top();
-}
-
-void save_value(void)
-{
-	cout << "Saving in " << buffer[0] << " (" << string(buffer) << ")" << endl;
-	MEMORY(*buffer) = eval(buffer + 2);
 }
 
 int main(void)
 {
 	memset(memory, 0, sizeof(memory));
-	while (scanf("%s", buffer) == 1) {
+	while (scanf("%s", buffer) == 1) 
 		if (buffer[1] == '=')
-			save_value();
+			MEMORY(*buffer) = eval(buffer + 2);
 		else
 			printf("%.2f\n", eval(buffer));
-	}
 	return 0;
 }
 
